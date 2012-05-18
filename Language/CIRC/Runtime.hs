74a7114019f476cf7067508b4cffc56e4e125ea1
Tom Hawkins <tomahawkins@gmail.com>
2012-05-18 11:15:35 -0500

    Added idId runtime function.  Reved version.

--- Language/CIRC/Runtime.hs
+++ Language/CIRC/Runtime.hs
@@ -5,20 +5,23 @@ module Language.CIRC.Runtime
   , runCIRC
   , Id
   , newId
+  , idId
   ) where
 
 import Control.Monad.State
 
 -- | The CIRC transform monad.  Used to create fresh ids.
-type CIRC = State Int
+type CIRC = State (Int, [(String, Int)])
 
 -- | Evaluates a CIRC transform.
 evalCIRC :: CIRC a -> Int -> a
-evalCIRC = evalState
+evalCIRC a i = evalState a (i, [])
 
 -- | Evaluates a CIRC transform, also returning the fresh next id.
 runCIRC :: CIRC a -> Int -> (a, Int)
-runCIRC = runState
+runCIRC a i = (b, j)
+  where
+  (b, (j, _)) = runState a (i, [])
 
 -- | Identifiers.
 type Id = String
@@ -26,7 +29,17 @@ type Id = String
 -- | Produces a fresh id.
 newId :: CIRC Id
 newId = do
-  id <- get
-  put $ id + 1
-  return $ "__" ++ show id
+  (i, table) <- get
+  put (i + 1, table)
+  return $ "__" ++ show i
+
+-- | Returns a unqiue int for a given id.
+idId  :: Id -> CIRC Int
+idId name = do
+  (i, table) <- get
+  case lookup name table of
+    Nothing -> do
+      put (i + 1, (name, i) : table)
+      return i
+    Just i -> return i
 
